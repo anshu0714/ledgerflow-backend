330ad510080138b0ffba8e7eb7e41e12d55aadb2
anshu0714 <anshujha14072002@gmail.com>
2026-03-02 12:56:29 +0530

    Set default server port to 3000 if PORT env variable is undefined

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
